ea707c60b4194ab573e424fcca64c62fcdeca34b
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-13 16:47:32 +0530

    Aligning row in 1 line along with hover effect

--- netflix_clone/src/App.js
+++ netflix_clone/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+import "./App.css";
 import Row from "./Component/Row.js";
 import requests from "./requests";
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-    
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
 
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- netflix_clone/src/Component/Row.js
+++ netflix_clone/src/Component/Row.js
@@ -1,33 +1,37 @@
-import React,{useState, useEffect} from 'react';
-import axios from '../axios';
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "../CssFiles/Row.css";
+const base_url = "http://image.tmdb.org/t/p/original/";
 
-const base_url="http://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  //A code snippet which runs based on spesific condition
+  useEffect(() => {
+    //if [], run once when row loads and dont again
+    async function fetchData() {
+      // const request="https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=28";
+      // const request = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=35");
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-function Row({title , fetchUrl}){
-    const [movies, setMovies] = useState([]);
-    //A code snippet which runs based on spesific condition
-    useEffect(() => {
-     //if [], run once when row loads and dont again
-        async function fetchData(){
-            // const request="https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=28";
-            // const request = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=35");
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
-
-    console.table(movies);
-    return(
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row__poster">
-                {movies.map(movie =>(
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-                ) )}
-            </div>
-        </div>
-    )
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Row;
