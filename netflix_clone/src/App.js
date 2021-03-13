7b9769a9e61b8ab184633a9f28c3755eabbcdfd4
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-13 23:14:18 +0530

    Compliting row part

--- netflix_clone/src/App.js
+++ netflix_clone/src/App.js
@@ -1,16 +1,23 @@
-import "./App.css";
+import "./CssFiles/App.css";
 import Row from "./Component/Row.js";
 import requests from "./requests";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
 
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
+      <Row title="NETFLIX ORIGINALS"
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
+  
     </div>
   );
 }
--- netflix_clone/src/Component/Row.js
+++ netflix_clone/src/Component/Row.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "../CssFiles/Row.css";
+
 const base_url = "http://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   //A code snippet which runs based on spesific condition
   useEffect(() => {
     //if [], run once when row loads and dont again
     async function fetchData() {
-      // const request="https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=28";
-      // const request = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=b8147d9a2b320232dcbd7689528ce05a&with_genres=35");
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
@@ -22,11 +21,13 @@ function Row({ title, fetchUrl }) {
   return (
     <div className="row">
       <h2>{title}</h2>
+
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarger"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
