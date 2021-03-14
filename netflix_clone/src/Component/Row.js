352baf28e6bce59d7d8651cf13eac062269eca52
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-14 16:34:24 +0530

    error solved now only trailer link is remaining

--- netflix_clone/src/Component/Row.js
+++ netflix_clone/src/Component/Row.js
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from "react";
-import YouTube from "react-youtube";
 import axios from "../axios";
 import "../CssFiles/Row.css";
-import movieTrailer from "movie-trailer";
 
 const base_url = "http://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
-
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
-  
   //A code snippet which runs based on spesific condition
   useEffect(() => {
     //if [], run once when row loads and dont again
@@ -22,31 +17,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-  const opts ={
-    height :"390",
-    width : "100%",
-    playerVars:{
-
-      autoplay:1,
-    },
-  };
-
-  const handleClick = (movie) => {
-    if(trailerUrl)
-    {
-      setTrailerUrl('');
-    }else{
-      movieTrailer(movie?.title || "")
-      .then((url) =>{
-        const urlParams = new URLSearchParams(new URL(url).search)
-        // setTrailerUrl(urlParams.get("v"))
-        setTrailerUrl("XtMThy8QKqU")
-      })
-      .catch((error) => console.log(error));
-    }
-  }
-
-
   // console.table(movies);
   return (
     <div className="row">
@@ -56,16 +26,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            onClick={()=> handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarger"}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
       </div>
-
-      { trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/> }  
-
     </div>
   );
 }
