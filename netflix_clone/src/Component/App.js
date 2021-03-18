4d253ce9d280b9e03d472c0d87956bb81e53be0f
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-19 00:30:54 +0530

    Completing Authentication

--- netflix_clone/src/Component/App.js
+++ netflix_clone/src/Component/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "../CssFiles/App.css";
 import HomeScreen from "../Screens/HomeScreen";
 import LoginScreen from "../Screens/LoginScreen"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from "../firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe=auth.onAuthStateChanged((userAuth) =>{
+      if(userAuth){
+        console.log(userAuth);
+      }else{
+
+      }
+      return unsubscribe;
+    });
+  }, []);
+
   return (
     <div className="app">
       {/* <HomeScreen /> */}
--- netflix_clone/src/firebase.js
+++ netflix_clone/src/firebase.js
@@ -1,4 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: "AIzaSyBEA9imAMJGq545RHmlmr0PQ_be3HjX80A",
     authDomain: "netflix-clone-c930a.firebaseapp.com",
@@ -8,3 +8,10 @@ const firebaseConfig = {
     appId: "1:350400695924:web:2308f954b7b4363bbf4b21",
     measurementId: "G-6HQ594GLRL"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db=firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {auth}
+  export default db;
