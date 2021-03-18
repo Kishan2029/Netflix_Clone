654d292c59355c0ac93627c5bff67df2829e53b3
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-19 01:13:00 +0530

    Trying to solve redux Provider error

--- netflix_clone/src/Component/App.js
+++ netflix_clone/src/Component/App.js
@@ -4,19 +4,29 @@ import HomeScreen from "../Screens/HomeScreen";
 import LoginScreen from "../Screens/LoginScreen"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from "../firebase";
+import {useDispatch} from "react-redux"
+import {login, logout} from "../userSlice"
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe=auth.onAuthStateChanged((userAuth) =>{
       if(userAuth){
+        //Logged In
         console.log(userAuth);
+        dispatch(login({
+          uid:userAuth.uid,
+          email: userAuth.email
+        }))
       }else{
-
+        //Logged Out
+        dispatch(logout);
       }
-      return unsubscribe;
+      
     });
+    return unsubscribe;
   }, []);
 
   return (
